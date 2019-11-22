272473b944237e36363fc308357cd6be1aacbe7e
ameliota <alessandro.meliota@datalogic.com>
2019-11-22 11:45:44 +0100

    add licence

--- muovi.py
+++ muovi.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import move_simple_it
+from move_simple_it import avanti, sinistra, indietro, stop
 
-move_simple_it.avanti(1)
-move_simple_it.sinistra(1)
-move_simple_it.indietro(5)
-move_simple_it.stop()
+avanti(1)
+sinistra(1)
+indietro(5)
+stop()
