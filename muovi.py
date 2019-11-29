3f5f5809ef4a4cfcd0eaeb6ea7930ef32315ef41
ameliota <alessandro.meliota@datalogic.com>
2019-11-29 17:32:19 +0100

    fix method name in italian

--- muovi.py
+++ muovi.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from move_simple_it import avanti, sinistra, indietro, stop
+from move_simple_it import avanti, sinistra, indietro, ferma
 
 avanti(1)
 sinistra(1)
 indietro(5)
-stop()
+ferma()
